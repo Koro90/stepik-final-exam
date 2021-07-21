a1e3f7f11f990ae232a4e812e1b941da3794d1ae
@koro90 <karina@goaxiomatic.com>
2021-07-21 09:09:52 -0400

    going between pages

--- pages/login_page.py
+++ pages/login_page.py
@@ -10,7 +10,7 @@ class LoginPage(BasePage):
 
     def should_be_login_url(self):
         # реализуйте проверку на корректный url адрес
-        assert "login" in browser.current_url, 'login url not presented'
+        assert "login" in self.browser.current_url, 'login url not presented'
 
     def should_be_login_form(self):
         # реализуйте проверку, что есть форма логина
